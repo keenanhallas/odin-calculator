186594aaf80e204ba9279837e46864667b653632
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-11-03 19:39:01 -0500

    Create operate function with working +, -, x, and / operators

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+function operate(num1, operator, num2){
+    switch(operator){
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "x":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        default:
+            console.log("Operator not recognized!");
+    }
+}
