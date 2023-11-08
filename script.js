3535578986f28e9e1e87154ffa7e8db9901bcda6
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-11-07 19:28:23 -0600

    Add functionality for keys to populate the screen & store screen content in a variable

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const calcState = {
     numberOne: 0,
     operator: "",
     numberTwo: 0,
+    screenContent: ``,
 };
+
+const screen = document.querySelector(".screen");
+screen.textContent = calcState.screenContent;
+const keyPad = document.querySelector(".keypad-container");
+keyPad.addEventListener("click", (e) => {
+    if(e.target.dataset.value){
+        calcState.screenContent += e.target.dataset.value;
+        screen.textContent = calcState.screenContent;
+        console.log(calcState.screenContent);
+    }
+});
 
 function operate(num1, operator, num2){
     switch(operator){
